ef53fd53c2fa898717b5c59bb144a100a0837efc
Lee-Siyoung <tldud4861@gmail.com>
2023-10-05 11:41:18 +0900

    기초 편집,삭제 보안 구현

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -14,9 +14,12 @@ class IdeaController extends Controller
 
     public function store()
     {
+        
         $validated = request()->validate([
             'content' => 'required|min:1|max:240'
         ]);
+
+        $validated['user_id']=auth()->id();
 
         Idea::create($validated);
         return redirect()->route('dashboard')->with('success', 'content created successfully!');
@@ -24,12 +27,18 @@ class IdeaController extends Controller
 
     public function edit(Idea $idea)
     {
+        if(auth()->id() !== $idea->user_id){
+            abort(404);
+        }
         $editing = true;
         return view('ideas.show',compact('idea', 'editing'));  // compact('idea') = ['idea'=>$idea]
     }
 
     public function update(Idea $idea)
     {
+        if(auth()->id() !== $idea->user_id){
+            abort(404);
+        }
         $validated = request()->validate([
             'content' => 'required|min:1|max:240'
         ]);
@@ -40,6 +49,9 @@ class IdeaController extends Controller
 
     public function destroy(Idea $idea)
     {
+        if(auth()->id() !== $idea->user_id){
+            abort(404);
+        }
         $idea->delete();
 
         return redirect()->route('dashboard')->with('success', 'content deleted successfully!');
--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -16,6 +16,7 @@ class Idea extends Model
     ]; */
 
     protected $fillable = [
+        'user_id',
         'content',
         'like',
     ];
@@ -23,5 +24,9 @@ class Idea extends Model
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
     }
 }
