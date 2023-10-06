5b1670a391c5db573be759cf0c43358007cadfe7
Lee-Siyoung <tldud4861@gmail.com>
2023-10-06 11:22:01 +0900

    feed 기능

--- resources/views/shared/left-sidebar.blade.php
+++ resources/views/shared/left-sidebar.blade.php
@@ -2,22 +2,25 @@
     <div class="card-body pt-3">
         <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
             <li class="nav-item">
-                <a class="{{ (Route::is('dashboard')) ? 'text-white bg-primary rounded' : '' }} nav-link" href="{{ route('dashboard') }}">
+                <a class="{{ Route::is('dashboard') ? 'text-white bg-primary rounded' : '' }} nav-link"
+                    href="{{ route('dashboard') }}">
                     <span>Home</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="{{ Route::is('feed') ? 'text-white bg-primary rounded' : '' }} nav-link"
+                    href="{{ route('feed') }}">
+                    <span>Feed</span></a>
             </li>
             <li class="nav-item">
                 {{-- web.php에서 name을 설정안했을때 쓰는 방법 
                     <a class="{{ (Request::is('terms')) ? 'text-white bg-primary rounded' : '' }} nav-link" href="{{ url('terms') }}">  --}}
-                <a class="{{ (Route::is('terms')) ? 'text-white bg-primary rounded' : '' }} nav-link" href="{{ route('terms') }}"> 
+                <a class="{{ Route::is('terms') ? 'text-white bg-primary rounded' : '' }} nav-link"
+                    href="{{ route('terms') }}">
                     <span>Terms</span></a>
             </li>
             {{-- <li class="nav-item">
                 <a class="nav-link" href="#">
                     <span>Explore</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span>Feed</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
@@ -27,6 +30,8 @@
                 <a class="nav-link" href="#">
                     <span>Settings</span></a>
             </li> --}}
+
+
         </ul>
     </div>
     <div class="card-footer text-center py-2">
